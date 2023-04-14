033308dde36113baaecbb33e57ce8e22dd9b6852
jamesfromonline <jtwebco@gmail.com>
2023-04-14 15:59:53 -0400

    more cleanup

--- components/VariantPicker.tsx
+++ components/VariantPicker.tsx
@@ -1,35 +1,10 @@
-import Image from "next/image"
-import { useState } from "react"
-
 const VariantPicker = ({ variants, ...props }) => {
   if (variants.length === (0 || 1)) return null
-
-  // const [activeVariant, setActiveVariant] = useState({
-  //   name: "",
-  //   size: "",
-  //   externalId: ""
-  // })
 
   const selectProps = { ...props }
 
   delete selectProps.activeVariant
   delete selectProps.changeVariant
-
-  const filteredVariants = []
-
-  variants.map((variant) => {
-    const variantName = variant.name.split("/")[0].trim()
-
-    if (filteredVariants.some((variant) => variant.name === variantName)) return
-
-    filteredVariants.push({
-      name: variantName,
-      external_id: variant.external_id,
-      files: variant.files
-    })
-
-    return null
-  })
 
   return (
     <div>
@@ -48,39 +23,6 @@ const VariantPicker = ({ variants, ...props }) => {
           )
         })}
       </select>
-      {/* <div
-        className={`grid grid-cols-3 gap-2 place-items-center w-full  mt-4 child:p-2`}
-      >
-        {filteredVariants.map((variant) => {
-          const { name, files, external_id } = variant
-          const isActiveVariant = props.activeVariant === name
-          return (
-            <div
-              role="button"
-              onClick={() => props.changeVariant(external_id)}
-              key={name}
-              className={`w-full relative group border-4 cursor-pointer ${
-                isActiveVariant ? "border-secondary" : "border-black"
-              }`}
-            >
-              <div
-                className={`z-10 absolute top-0 left-0 bg-black text-white grid place-items-center w-full h-full ${
-                  isActiveVariant
-                    ? "opacity-100"
-                    : "opacity-0 group-hover:opacity-100"
-                }  transition duration-200 ease-in-out`}
-              >
-                <p className="lowercase text-xs md:text-sm font-bold">{name}</p>
-              </div>
-              <div className="w-full h-full">
-                <div className="relative w-20 h-20 mx-auto">
-                  <Image src={files[1].preview_url} alt={name} fill />
-                </div>
-              </div>
-            </div>
-          )
-        })}
-      </div> */}
     </div>
   )
 }
--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -1,42 +1,42 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next"
 
-import { printful } from "lib/printful-client";
+import { printful } from "lib/printful-client"
 
 type Data = {
-  id: string;
-  price: number;
-  url: string;
-};
+  id: string
+  price: number
+  url: string
+}
 
 type Error = {
-  errors: { key: string; message: string }[];
-};
+  errors: { key: string; message: string }[]
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
-  const { id } = req.query;
+  const { id } = req.query
 
   try {
-    const { result } = await printful.get(`store/variants/@${id}`);
+    const { result } = await printful.get(`store/variants/@${id}`)
 
-    res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
+    res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate")
 
     res.status(200).json({
       id: id as string,
       price: result.retail_price,
-      url: `/api/products/${id}`,
-    });
+      url: `/api/products/${id}`
+    })
   } catch ({ error }) {
-    console.log(error);
+    console.log(error)
     res.status(404).json({
       errors: [
         {
           key: error?.message,
-          message: error?.message,
-        },
-      ],
-    });
+          message: error?.message
+        }
+      ]
+    })
   }
 }
